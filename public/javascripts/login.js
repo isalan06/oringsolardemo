b4298abd1f19c7c6bb628b3a2030a6b9f99f9908
isalan06 <isalan06@gmail.com>
2021-05-24 06:59:52 +0000

    Update

---
+++ public/javascripts/login.js
@@ -0,0 +1,6 @@
+const button_login = document.getElementById('btnRegister');
+button_login.addEventListener('click', function(e){
+	fetch('/PostTest2', {method: 'POST'})
+	;
+	});
+});
---
+++ routes/index.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql');
+
+var config =
+	{
+		host: '34.80.153.164',
+		user: 'root',
+		password: '!QAZxsw2',
+		database: 'Solar_DB',
+		port: 3306
+	};
+
+//const conn = new mysql.createConnection(config);
+
+/* GET home page. */
+router.get('/', function(req, res) { //, next) {
+  //res.render('index', { title: 'Express' });
+  res.render('login', {
+  	title: 'Oring Solar Demo',
+	  showMessage: ''
+  });
+});
+
+router.get('/Test', function(req, res){
+  res.send('API Test');
+});
+
+router.post('/PostTest', function(req, res){
+  res.send('POST Test');
+});
+router.post('/PostTest2', function(req, res){
+  res.send('POST Test 2');
+});
+
+
+router.post('/CheckUser', function(req, res){
+  var organ = req.body['txtOrgan'],
+  	user = req.body['txtUser'],
+	pass = req.body['txtPass'];
+  if( organ === 'Test' && user === 'Test' && pass === 'Test'){
+  	res.send('Check User Success');
+  }
+  else{
+    //res.send('Check User Failure');
+    	res.render('login', {
+		title: 'ORing Solar Demo',
+		showMessage: '身分驗證錯誤'
+	});
+  }
+
+});
+
+router.get('/TestDB', function(req, res){
+  const conn = new mysql.createConnection(config);
+	conn.connect(
+	  function(err){
+	  	if(err){
+			conn.end();
+			res.send('Connect DB Error');
+		}
+		else
+		{
+		  //conn.query('SELECT * FROM table_solar_current',
+		  conn.query('CALL pro_get_summary();',
+			  function(err, rows){
+			  	if(err) res.send('Get Data Error');
+				else{
+					var length = Object.keys(rows).length;
+					var resultString = 'Selected ' + length + ' row(s)\n';
+					rows[0].forEach( (row) => {
+						resultString += row['inverter_id'];
+						resultString += " - ";
+						resultString += JSON.stringify(row);
+					}
+					);
+				
+					conn.end();
+					res.send(resultString);
+				}
+			  });
+		  
+		}
+	  }
+  );
+});
+
+function readData()
+{
+  var  returnString = 'Connect DB Success2';
+
+  conn.query('SELECT * from table_solar_current',
+	  function(err, results){
+	    if(err) returnString = 'Get Data Error';
+	    else {
+	      returnString = 'Selectd ' + results.length + ' row(s)';
+	    }
+		 
+	  }
+  );
+  //returnString = 'ABC';
+  return returnString;
+};
+
+module.exports = router;
