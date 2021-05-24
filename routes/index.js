6c024e3df322e9a4f4331dffd02e5a5ef4057da0
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-05-24 15:30:15 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,11 @@ router.post('/PostTest2', function(req, res){
   res.send('POST Test 2');
 });
 
+router.post('/Information', function(req, res){
+	res.render('information', {
+		title: 'Oring Solar Demo - Information'
+	});
+});
 
 router.post('/CheckUser', function(req, res){
   var organ = req.body['txtOrgan'],
