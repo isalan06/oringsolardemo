954bbc1232fa60f9c3a051c77c30c3b4fb917479
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-05-25 11:06:01 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -82,8 +82,8 @@ router.get('/Summary', function(req, res){
   	);
 	res.render('summary', {
 		title: 'Oring Solar Demo - Summary',
-		i_online_count: online_count,
-		i_total_count: total_count,
+		i_online_count: String(online_count),
+		i_total_count: '20',
 		i_today_energy: today_energy
 	  });
 });
