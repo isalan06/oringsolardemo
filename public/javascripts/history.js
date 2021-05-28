d7e0525ce95d373123ba72b99b4621ad9abed260
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-05-28 11:34:35 +0800

    update

---
+++ public/javascripts/history.js
@@ -0,0 +1,9 @@
+function prefixZeros(number, maxDigits) 
+{  
+    var length = maxDigits - number.toString().length;
+    if(length <= 0)
+        return number;
+
+    var leadingZeros = new Array(length + 1);
+    return leadingZeros.join('0') + number.toString();
+}
