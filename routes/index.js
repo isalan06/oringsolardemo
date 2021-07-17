02312443ba5c7879bf00566201e1d3cc0666eea0
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-07-17 08:42:29 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -240,7 +240,7 @@ router.get('/SolarLocation', function(req, res) {
 								today_hour_energy = 0;
 							}
 							console.log(rows);
-							console.log(rows[0]['Total_Energy']);
+							console.log(rows[0][0]['RowDataPacket']['Total_Energy']);
 							console.log(rows[1]['Total_Energy']);
 
 							conn.end();
