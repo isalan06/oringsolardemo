dd201b4a3ebc5db1e8923d13b2106747b58a47a9
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-08-23 11:41:31 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -894,6 +894,7 @@ router.post('/SolarHistory', function(req, res){
 					
 										var energyDataString = JSON.stringify(energyData);
 
+										console.log(energyDataString);
 										res.render('solarhistory', {
 											title: 'Oring Solar System Demo - History',
 											setsublocationindex:1,
