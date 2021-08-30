28ba4b53eb1ac7f06eee5df8b0595256cae14c77
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-08-30 14:50:21 +0800

    update

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.bodyParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
