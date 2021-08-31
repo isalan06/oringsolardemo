8b125d613b626667e2ed01612e2e93e04d391d33
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-08-31 19:30:22 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2565,7 +2565,7 @@ router.get('/Excel2', function(req, res){
 
 router.post('/ExportExcel', function(req, res){
 	console.log('Export Excel');
-	console.log(req.body);
+	console.log(req.body.tabledata);
 
 
 	res.send('Test');
