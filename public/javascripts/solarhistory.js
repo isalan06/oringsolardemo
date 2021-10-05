2b17244b87046b803d8044fd5900dc54f85e8edb
Strawberry Huang <alan.huang6@msa.hinet.net>
2021-10-05 14:36:30 +0800

    update

--- public/javascripts/solarhistory.js
+++
@@ -1,6 +0,0 @@
-function SelectAll(){
-    var getInverter = document.getElementsByName("checkInverter");
-    for(var i=0;i<getInverter.length;i++){
-        getInverter[i].checked = true;
-    }
-}
