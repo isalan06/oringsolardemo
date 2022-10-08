782755683a9b8199a839dc9fcc04b7a5b2b6394a
isalan06 <isalan06@gmail.com>
2022-10-08 09:09:57 +0800

    update

--- routes/gateway/getTokenByMacAddress.js
+++ routes/gateway/getTokenByMacAddress.js
@@ -21,7 +21,7 @@ const getTokenByMacAddress=(req, res)=>{
                         var token = rows[0]['token'];
                         var usecloud = rows[0]['usecloud'];
                         var usertoken = rows[0]['usertoken'];
-                        var cloudurl = rows[0]['cloudrul'];
+                        var cloudurl = rows[0]['cloudurl'];
                         var cloudtype = rows[0]['cloudtype'];
                         console.log(token);
                         outputData={};
@@ -37,7 +37,12 @@ const getTokenByMacAddress=(req, res)=>{
                         res.send(JSON.stringify(outputData));
                     }
                     else{
-                        conn.end(); errordata={}; errordata['result']=3; errordata['errordescription']='There is no Mac Address (' + macaddress + ") in system." ;  res.send(JSON.stringify(errordata));
+                        
+                        commandString='INSERT INTO table_unregistermacaddress VALUES(NOW(), \'' + macaddress + '\');';
+                        conn.query(commandString, function(err, rows){
+                            conn.end(); errordata={}; errordata['result']=3; errordata['errordescription']='There is no Mac Address (' + macaddress + ") in system." ;  res.send(JSON.stringify(errordata));
+                        
+                        });
                     }
                 }
             });
